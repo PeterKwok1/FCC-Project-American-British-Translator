9965a64286889590d6de12555b0c354d7dbace75
Peter Kwok <peter.kwok94@gmail.com>
2024-04-26 14:14:30 -0400

    feat: setup unit and functional tests

--- routes/api.js
+++ routes/api.js
@@ -3,11 +3,11 @@
 const Translator = require('../components/translator.js');
 
 module.exports = function (app) {
-  
+
   const translator = new Translator();
 
   app.route('/api/translate')
     .post((req, res) => {
-      
+      // console.log(req.body)
     });
 };
